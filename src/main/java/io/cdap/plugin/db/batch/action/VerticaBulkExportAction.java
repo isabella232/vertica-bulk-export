5b3d992341ff4d5cda9b13a4fd95d77678ee20e7
sagarkapare <sagarkapare@google.com>
2019-04-17 16:07:52 -0700

    Update for 6.0

--- src/main/java/io/cdap/plugin/db/batch/action/VerticaBulkExportAction.java
+++ src/main/java/io/cdap/plugin/db/batch/action/VerticaBulkExportAction.java
@@ -14,15 +14,15 @@
  * the License.
  */
 
-package co.cask.hydrator.plugin.db.batch.action;
+package io.cdap.plugin.db.batch.action;
 
-import co.cask.cdap.api.annotation.Description;
-import co.cask.cdap.api.annotation.Macro;
-import co.cask.cdap.api.annotation.Name;
-import co.cask.cdap.api.annotation.Plugin;
-import co.cask.cdap.api.plugin.PluginConfig;
-import co.cask.cdap.etl.api.action.Action;
-import co.cask.cdap.etl.api.action.ActionContext;
+import io.cdap.cdap.api.annotation.Description;
+import io.cdap.cdap.api.annotation.Macro;
+import io.cdap.cdap.api.annotation.Name;
+import io.cdap.cdap.api.annotation.Plugin;
+import io.cdap.cdap.api.plugin.PluginConfig;
+import io.cdap.cdap.etl.api.action.Action;
+import io.cdap.cdap.etl.api.action.ActionContext;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
